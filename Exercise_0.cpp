a013c81559865c53dd0992a48fe3ebaac884f6f9
SuperDryHan <150392839+SuperDryHan@users.noreply.github.com>
2023-11-10 08:25:07 +1100

    Add files via upload
    Exercise_3 need to be polished.

---
+++ Exercise_0.cpp
@@ -0,0 +1,152 @@
+// Ex 0-0
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+// Ex 0-1
+#include <iostream>
+
+int main()
+{
+    3+4;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+// Ex 0-2
+#include <iostream>
+
+int main()
+{
+    std::cout << "This (\") is a quote, and this (\\) is a backslash." << std::endl;
+}
+
+
+
+
+
+
+
+
+
+
+// Ex 0-3
+#include <iostream>
+
+int main()
+
+{
+    std::cout << "aa\taa" << std::endl;
+}
+
+
+
+
+
+
+
+
+
+
+// Ex 0-4
+#include <iostream>
+
+int main()
+
+{
+    std::cout << "#include <iostream>" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "int main()" << std::endl;
+    std::cout << "{" << std::endl;
+    std::cout << "    std::cout << \"Hello, World!\" << std::endl;" << std::endl;
+    std::cout << "    return 0;" << std::endl;
+    std::cout << "}" << std::endl;
+}
+
+
+
+
+
+
+
+
+
+
+// EX 0-5
+// Invlid code as curly brakets are missing.
+
+
+
+
+
+
+
+
+
+
+// EX 0-6
+// Valid as units inside a unit are valid.
+
+
+
+
+
+
+
+
+
+
+// EX 0-7
+// Invalid. The comment is used in an invalid manner as comment block in the middle of the comment is not used with exception. 
+
+
+
+
+
+
+
+
+
+
+
+// EX 0-8
+// Valid, as after two slashes whatever follows is considered as comment.
+
+
+
+
+
+
+
+
+
+
+// EX 0-9
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello,\nWorld!" << std::endl;
+    return 0;
+}
